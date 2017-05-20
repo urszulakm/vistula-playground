41ebca4a792c3ffd53db09b308c2e3defe1a8848
slawekradzyminski <slawomir.radzyminski@gmail.com>
2017-05-20 20:55:06 +0200

    fixes

--- src/main/java/org/vistula/collections/CredentialsValidator.java
+++ src/main/java/org/vistula/collections/CredentialsValidator.java
@@ -6,19 +6,4 @@ public class CredentialsValidator {
 
     private HashMap<String, String> credentials;
 
-    public CredentialsValidator(HashMap<String, String> credentials) {
-        this.credentials = credentials;
-    }
-
-    public HashMap<String, String> getCredentials() {
-        return credentials;
-    }
-
-    public void setCredentials(HashMap<String, String> credentials) {
-        this.credentials = credentials;
-    }
-
-    public boolean areCredentialsValid(String login, String password) {
-        return true;
-    }
 }
